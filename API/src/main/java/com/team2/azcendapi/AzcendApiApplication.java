7cf7be90336014343596712f0a87fb8a1a41d541
Chintan Jariwala <CJariwala-consultant@Scholastic.com>
2017-10-21 13:23:33 -0700

    Added files for API repo

---
+++ API/src/main/java/com/team2/azcendapi/AzcendApiApplication.java
@@ -0,0 +1,12 @@
+package com.team2.azcendapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzcendApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzcendApiApplication.class, args);
+	}
+}
