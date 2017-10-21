662b93aee03d7d6d41a7bbcffaaaffa5d42ad1bc
Jake Folsom <jfolsom2@asu.edu>
2017-10-21 16:01:05 -0700

    Added login page using bootstrap

--- App/src/app/app.module.ts
+++ App/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
